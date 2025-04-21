587df41a0bbbe4079726b3d14eea626e7db0df66
JAMES9696 <55827957+JAMES9696@users.noreply.github.com>
2025-04-21 04:24:40 -0700

    Copy worldlora-search/src/app/search/page.tsx from star

---
+++ worldlora-search/src/app/search/page.tsx
@@ -0,0 +1,11 @@
+// Placeholder for the main search page
+// Logic and UI components will be integrated here according to the workflow
+
+export default function SearchPage() {
+  return (
+    <div>
+      <h1>搜索页面</h1>
+      {/* 在这里集成从 v0.dev 获取的搜索 UI 组件和相关逻辑 */}
+    </div>
+  );
+} 
